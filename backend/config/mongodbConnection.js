bb5d43a91840785ae9d4918a046cce8ce1877ffb
looh16 <langacustodio@gmail.com>
2023-02-02 12:36:45 +0200

    Add configurations to connect backend with Mongo DB

---
+++ backend/config/mongodbConnection.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser : true})
+
+const connection = mongoose.connection
+
+connection.on('error', (error) => {
+    console.log(error)
+});
+
+connection.on("connected", () => {
+    console.log('Mongo DB connection successfull')
+})
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const mongodbConnection = require("./config/mongodbConnection")
 const port = 5000;
 
-app.get('/', (req, res) => res.send('Hello Word!'))
-app.listen(port, () => console.log(`Server is Running on Port ${port}!`))
+app.get('/', (req, res) => res.send('Hello Word!'));
+app.listen(port, () => console.log(`Server is Running on Port ${port}!`));
