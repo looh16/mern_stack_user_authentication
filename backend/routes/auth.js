677d6ad7acb1a7ffb6efbeb1b33423156314139e
looh16 <langacustodio@gmail.com>
2023-02-02 13:16:46 +0200

    Add endpoints to user register and login

---
+++ backend/routes/auth.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/userModel");
+
+router.post("/register", async (req, res) => {
+    try {
+        const newUser = new User(req.body);
+        await newUser.save();
+        res.status(200).send({ sucess: true, message: "User Registed SUccessfully" })
+
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({
+            email: req.body.email,
+            password: req.body.password,
+        });
+        if (user) {
+            res.status(200).send({ sucess: true, message: "User Login SUccessfully", data: user })
+        } else {
+            res.status(200).send({ sucess: true, message: "User Login Failed", data: null })
+        }
+
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+module.exports = router
+
+
--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,11 @@ const express = require('express');
 require('dotenv').config();
 
 const app = express();
+app.use(express.json)
+const authRoute = require("./routes/auth")
 const mongodbConnection = require("./config/mongodbConnection")
 const port = 5000;
+app.use('api/auth', authRoute)
 
 app.get('/', (req, res) => res.send('Hello Word!'));
 app.listen(port, () => console.log(`Server is Running on Port ${port}!`));
