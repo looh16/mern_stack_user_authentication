fe449a238cf3041c5c557b8e67d3c77966ac8194
looh16 <langacustodio@gmail.com>
2023-02-05 12:28:24 +0200

    Add middleware and user endpoint

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import axios from "axios";
 
-function Home() {
+const Home = () => {
+  const [userInfo, setUserInfo] = useState(null);
+  const navigate = useNavigate();
+
+  const getData = async () => {
+    toast.loading();
+    try {
+      const token = localStorage.getItem("user");
+      const response = await axios.get("/api/user/get-user-info", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      toast.dismiss();
+      if (response.data.success) {
+        setUserInfo(response.data.data);
+      } else {
+        localStorage.removeItem("user");
+        navigate("/login");
+        toast.error("Something went wrong");
+      }
+    } catch (error) {
+      localStorage.removeItem("user");
+      navigate("/login");
+      toast.error("Something went wrong");
+    }
+  };
+
+  useEffect(() => {
+    if (userInfo == null) {
+      getData();
+    }
+  }, [userInfo]);
+
+
   return (
     <div>Home</div>
   )
